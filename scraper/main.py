35052ee6b263c041239c0d7ef949e1e0b2d2e702
Omar Besbes <86571415+omar-besbes@users.noreply.github.com>
2024-05-02 22:00:14 +0100

    [Feat] setup scraper

--- scraper/main.py
+++ scraper/main.py
@@ -1,9 +1,12 @@
+import json
+import logging
 import os
+import time
+from typing import Dict, Optional
+from urllib.parse import urljoin, urlencode
+
 import requests
 from confluent_kafka import Producer
-import time
-import logging
-from typing import Optional, Dict
 from dotenv import load_dotenv
 
 # Load environment variables from .env file
@@ -24,6 +27,7 @@ required_env_vars: Dict[str, str] = {
     'KAFKA_TOPIC': os.environ.get('KAFKA_TOPIC'),
     'API_URL': os.environ.get('API_URL'),
     'API_KEY': os.environ.get('API_KEY'),
+    'POLLING_INTERVAL': os.environ.get('POLLING_INTERVAL'),
 }
 
 # Load environment variables
@@ -44,9 +48,14 @@ headers = {
     'X-Auth-Token': required_env_vars['API_KEY']
 }
 
-def fetch_data_from_api() -> Optional[Dict]:
+def fetch_data_from_api(date: str) -> Optional[Dict]:
     try:
-        response = requests.get(required_env_vars['API_URL'], headers=headers)
+        # Build the endpoint URL with the current date parameter
+        endpoint = urljoin(required_env_vars['API_URL'], '/v4/matches')
+        query_params = {'status': 'IN_PLAY'}
+        url = f"{endpoint}?{urlencode(query_params)}"
+
+        response = requests.get(url, headers=headers)
         response.raise_for_status()  # Raise an exception for non-2xx status codes
         return response.json()
     except Exception as e:
@@ -55,19 +64,24 @@ def fetch_data_from_api() -> Optional[Dict]:
 
 def send_to_kafka(data: Dict) -> None:
     try:
+        # Serialize the dictionary to JSON and encode it to bytes
+        serialized_data = json.dumps(data).encode('utf-8')
         producer = Producer(producer_conf)
-        producer.produce(required_env_vars['KAFKA_TOPIC'], value=data)
+        producer.produce(required_env_vars['KAFKA_TOPIC'], value=serialized_data)
         producer.flush()
         logger.info("Data sent to broker successfully")
     except Exception as e:
         logger.error(f"Error sending data to broker: {e}")
 
 def main() -> None:
+    interval = float(required_env_vars['POLLING_INTERVAL'])
     while True:
-        data = fetch_data_from_api()
+        # Get the current date in the desired format (e.g., YYYY-MM-DD)
+        current_date = time.strftime("%Y-%m-%d", time.localtime())
+        data = fetch_data_from_api(current_date)
         if data:
             send_to_kafka(data)
-        time.sleep(60)  # Polling interval of 60 seconds
+        time.sleep(interval)
 
 if __name__ == "__main__":
     main()
