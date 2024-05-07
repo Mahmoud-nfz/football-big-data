3b9c2625cf2f34731c286bdd2cee75ec29c00159
Mahmoud <mahmoudhnefzi@gmail.com>
2024-05-07 11:40:55 +0200

    [Fix] Unix line endings in start-namenode.sh

--- hadoop/scripts/start-namenode.sh
+++ hadoop/scripts/start-namenode.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# If you are on windows and this script does not work, it might be due to windows vs unix line endings
+# Run the following command in git bash to convert this script to unix endings
+# sed -i -e 's/\r$//' start-namenode.sh
+
 hdfs namenode &
 
 # copy required data directories
@@ -9,5 +13,6 @@ done
 echo "Created /data/football-events hdfs dir"
 hdfs dfs -put /opt/hadoop/data/* /data/football-events
 echo "Created /data/football-events hdfs dir"
+
 
 wait
